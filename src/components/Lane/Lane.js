9836f510c845b39f015bf66366415ba8fed42fea
novem <novem@Novem-MBP.local>
2020-08-08 14:39:05 +0900

    first commit

---
+++ src/components/Lane/Lane.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Ticket from '../Ticket/Ticket';
+import styled from 'styled-components';
+
+const LaneWrapper = styled.div`
+  list-style: none;
+  text-align: left;
+  padding: 0;
+  margin: 2%;
+  background: lightGray;
+  border-radius: 20px;
+  min-height: 50vh;
+  width: 40%;
+
+  @media (max-width: 768px) {
+    margin-bottom: 5%;
+    width: 100%;
+  }
+`;
+
+const Title = styled.h2`
+  width: 100%;
+  padding-bottom: 10px;
+  text-align: center;
+  border-bottom: 1px solid darkGray;
+`;
+
+const TicketsWrapper = styled.div`
+padding: 5%;
+`;
+
+const Alert = styled.div`
+text-align: center;
+`;
+
+const Lane = ({ tickets, loading,error,title }) => (
+  <LaneWrapper>
+    <Title>{title}</Title>
+    {(loading || error) && <Alert>{loading ? 'Loading...' :
+      error}</Alert>}
+    <TicketsWrapper>
+      {tickets.map(ticket => <Ticket key={ticket.id}
+        ticket={ticket} />)}
+    </TicketsWrapper>
+  </LaneWrapper>
+);
+
+export default Lane;
---
+++ src/containers/Board.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import withDataFetching from '../withDataFetching';
+import Lane from '../components/Lane/Lane';
+
+const BoardWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: 5%;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      loading: true,
+      error: '',
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const tickets = await fetch('../../assets/data.json');
+      const ticketsJSON = await tickets.json();
+
+      if (ticketsJSON) {
+        this.setState({
+          data: ticketsJSON,
+          loading: false,
+        });
+      }
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: error.message,
+      });
+    }
+  }
+
+  render() {
+    const { data, loading, error } = this.state;
+
+    const lanes = [
+      { id: 1, title: 'To Do' },
+      { id: 2, title: 'In Progress' },
+      { id: 3, title: 'Review' },
+      { id: 4, title: 'Done' },
+    ];
+
+    return (
+      <BoardWrapper>
+        {lanes.map(lane => (
+          <Lane
+            key={lane.id}
+            title={lane.title}
+            loading={loading}
+            error={error}
+            tickets={data.filter(ticket => ticket.lane ===
+              lane.id)}
+          />
+        ))}
+      </BoardWrapper>
+    );
+  }
+}
+
+export default Board;
